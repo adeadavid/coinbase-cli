f1a2aaae1986b9d93621638ba595412c1cd4b0ea
Adegorite Adolabi David <adeadavid@gmail.com>
2021-03-21 00:03:21 +0100

    :art: MODIFY: set up price command options and action that calls the getData function from the Api library

--- bin/coinbase-check.js
+++ bin/coinbase-check.js
@@ -1,10 +1,26 @@
 #!/usr/bin/env node
 const program = require("commander");
+const Configstore = require("configstore");
+const Api = require("../lib/Api");
 
+const store = new Configstore();
+const api = new Api(store.get("API-KEY"));
 program
   .command("price")
   .alias("p")
+  .option(
+    "--base <baseCurrency>",
+    "set the base currency/s using csv defaults to 'BTC,ETH,XRP'",
+    "BTC,ETH,XRP"
+  )
+  .option(
+    "--quote <quoteCurrency>",
+    "set the quote currency/s using csv defaults to 'USD'",
+    "USD"
+  )
   .description("Check the status of cyptocurreny price, DEFAULT:BTC")
-  .action(() => console.info("fROM CKECK PRICE"));
+  .action((options) => {
+    api.getData(options.base, options.quote);
+  });
 
 program.parse(process.argv);
