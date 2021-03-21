a0f11fafce8ba3df76a195914929ad4150d0632f
Adegorite Adolabi David <adeadavid@gmail.com>
2021-03-21 14:44:03 +0100

    :recycle: MODIFY: to show red on negative values and green on positive

--- utils/responseView.js
+++ utils/responseView.js
@@ -8,12 +8,26 @@ function responseView(coin, logo, quote) {
       formatCurrency(coin.price, quote).green
     }\n${"High".yellow}: ${formatCurrency(coin.high, quote).green}\n${
       "1 Day Price change".yellow
-    }: ${formatCurrency(coin["1d"].price_change, quote).green} (${
-      coin["1d"].price_change_pct.green
-    }${"%".green})\n${"30 Days Price change".yellow}: ${
-      formatCurrency(coin["30d"].price_change, quote).green
-    } (${coin["30d"].price_change_pct.green}${
-      "%".green
+    }: ${
+      coin["1d"].price_change >= 0
+        ? formatCurrency(coin["1d"].price_change, quote).green
+        : formatCurrency(coin["1d"].price_change, quote).red
+    } (${
+      coin["1d"].price_change_pct >= 0
+        ? coin["1d"].price_change_pct.green
+        : coin["1d"].price_change_pct.red
+    }${coin["1d"].price_change_pct >= 0 ? "%".green : "%".red})\n${
+      "30 Days Price change".yellow
+    }: ${
+      coin["30d"].price_change >= 0
+        ? formatCurrency(coin["30d"].price_change, quote).green
+        : formatCurrency(coin["30d"].price_change, quote).red
+    } (${
+      coin["30d"].price_change_pct >= 0
+        ? coin["30d"].price_change_pct.green
+        : coin["30d"].price_change_pct.red
+    }${
+      coin["30d"].price_change_pct >= 0 ? "%".green : "%".red
     })\n---------------------------------------------------\n`
   );
 }
